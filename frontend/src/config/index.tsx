c583f5525cadcae579791fe1b977199e7fd76224
liram11 <liram07@gmail.com>
2022-11-06 22:03:49 +0300

    some clean up

--- frontend/src/config/index.tsx
+++ frontend/src/config/index.tsx
@@ -1,2 +1,2 @@
-export const BASE_URL: string = '';
-export const MASTER_URL: string = '/api/v1/paper/';
+export const BASE_URL: string = 'https://untitled1.community.saturnenterprise.io:8000/';
+export const MASTER_URL: string = 'https://untitled1.community.saturnenterprise.io:8000/api/v1/paper/';
