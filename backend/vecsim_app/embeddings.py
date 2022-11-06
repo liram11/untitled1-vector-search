c36ea2d91c10ab9056785ee5680ccef058283074
artem-mlops <artem@mlops.community>
2022-11-06 05:26:51 +0000

    re-embeddings

--- backend/vecsim_app/embeddings.py
+++ backend/vecsim_app/embeddings.py
@@ -5,7 +5,7 @@ from sentence_transformers import SentenceTransformer
 
 
 class Embeddings:
-    def __init__(self, model_name: str = "sentence-transformers/all-mpnet-base-v2"):
+    def __init__(self, model_name: str = "sentence-transformers/all-distilroberta-v1"):
         self.model_name = model_name
         self.model = SentenceTransformer(self.model_name)
 
