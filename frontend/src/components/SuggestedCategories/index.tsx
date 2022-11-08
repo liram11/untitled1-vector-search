eee35f0c2ae59412aefcc8a89ae8d94f07022e25
Artem Yushkovskiy <artem.yushkovskiy@deliveryhero.com>
2022-11-08 01:10:18 +0100

    Expand names in categories

--- frontend/src/components/SuggestedCategories/index.tsx
+++ frontend/src/components/SuggestedCategories/index.tsx
@@ -1,11 +1,14 @@
 import { Link } from "@mui/material"
+import { CATEGORY_HUMAN_NAMES } from '../../constants/search_filter';
 
 export const SuggestedCategories = ({options, onClick}: {options: string[], onClick: () => void}) => {
   if (options.length === 0) return <></>
 
   return (
     <div className="pt-2">
-      We found suggested categories for your query: <br /> {options.join(', ')} <br />
+      <a style={{backgroundColor: "#FFD801"}}>We found suggested categories for your query:</a> <br /> {options.map(slug =>
+        <div>{slug} ({CATEGORY_HUMAN_NAMES[slug as keyof typeof CATEGORY_HUMAN_NAMES]})</div>
+      )} <br />
       <Link component="button" underline="hover" onClick={onClick}>Apply them</Link>
     </div>
   )
